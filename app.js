c9ef2fabc574b596dd97b0b8a6d3834a13957178
Umer-FarooqAhmed <123248058+Umer-Farooq-Ahmed@users.noreply.github.com>
2023-02-20 01:15:15 +0500

    javaScript.file
    javaScript.file

---
+++ app.js
@@ -0,0 +1,162 @@
+// ***** CHAPTER NO14to16
+// // Question no1
+// var studentsNames=[]
+
+// Question no2
+// var studentsNames=[,]
+
+
+// // Question no3
+// var studentsNames=['muhammad','farooq']
+// console.log(studentsNames)
+
+
+// Question no4
+// var num1=['12','13']
+// console.log(num1)
+
+
+// // Question no5
+// var boolean=[true,flase]
+// console.log(boolean)
+
+
+// Question no6
+// var mixedArray = [1, "Bob", "Now is", true];
+// console.log(mixedArray)
+
+// Question no7
+// var names=("education qualifications in Pakistan")
+
+// var qualifications=["SSC","HSC", "BCS","BS"," BCOM", "MS", "M. Phil", "PhD"]
+// document.write(names)
+
+
+// document.write(qualifications)
+
+
+// Question no8
+// var student1=["sorce of Micheal is 320.percentage:64%"]
+// var student2=["sorce of john is 230.percentage:46%"]
+// var student3=["sorce of tony is 480.percentage:96%"]
+
+// document.write(student1)
+// document.write(student2)
+// document.write(student3)
+
+// / Question no9
+// var colorNames=("<h1>"+"colorNames"+"</h1>")
+// document.write(colorNames)
+
+// var color1=prompt("first color name")
+
+// document.write(color1)
+
+
+// var color2=prompt("last color name")
+
+// document.write(color2)
+
+
+// // Question no9
+// var student1=["sorces of student :320,280,480.120"]
+// var student1=["sorces of student :320,480,280,120"]
+
+// document.write(student1)
+
+// // Question no10
+// var citiesNames=["lahore",'islamabad','karchi',"hyderabad"]
+
+// document.write(citiesNames[0][1])
+
+// //  Question no11
+// var citiesNames=["lahore",'islamabad','karchi',"hyderabad"]
+
+// citiesNames[0]=citiesNames[0],citiesNames[3]=citiesNames[3]
+
+// console.log(citiesNames)
+
+// //  Question no12
+// var arr = ["This",  "is",  "my ",  "cat"][0];
+
+// console.log(arr)
+
+// //  Question no13
+// var arr = ['keyboard','mouse ','monitor','printer']
+// var arry=arr.shift()
+// console.log(arry)
+
+
+//  Question no14
+// var arr = ['keyboard','mouse ','monitor','printer']
+// var arry=arr.pop()
+// console.log(arry)
+
+// Question no15
+// var arr =('Apple', 'Samsung', 'Motorola',' Nokia', 'Sony & Haier')
+// document.write(arr)
+
+// *******                                 CHAPTER NO 17
+//                                                                 // QUESTION NO 1
+// var arrarr =[[]];
+// console.log(arrarr);
+
+                                                                // QUESTION NO 2
+// var arrarr =[[0 ,1 ,2 ,3],[1, 0 ,1 ,2],[2, 1, 0, 1]];
+// document.write(arrarr);                         
+
+                                                                // QUESTION NO 3
+//  for (var i= 0; i <=10; i++){
+//     document.write(i +'<br>')
+// }              
+
+//                                                                 // QUESTION NO 4
+// for(var i =1; i <= 12; i++){
+//     document.write('<h3>2*'+i +'='+ 2 * i +'</h3>')
+// }
+// var user_table =prompt ();
+// var user_lengt =prompt ()
+// document.write('<h3>Multiplication table = '+user_table+'<br>' +'Length = '+user_lengt+'</h3>')
+// for(var i = 1; i <= user_lengt; i++ ){
+//     document.write(user_table+"*"+i +'='+ user_table*i +'<br>')
+// }
+
+//                                                                             // QUESTION NO 5
+// var fruts = ['apple','banana','mango','orenge','stawberry']
+// for(var i = 0; i <= 4; i++){
+//     document.write("Element at index "+ i +" is "+ fruts [i] + "<br>" )
+//  }                 
+
+                                                                            // QUESTION NO 6
+// for(var i = 1; i <= 10; i++){
+//     document.write(i +',')
+// }
+
+                                                                            // QUESTION NO 7
+// var bakry_item = ['cake', 'apple pie', 'cookie', 'chips', 'patties'];
+// var user = prompt('Enter your food')  
+// var flag = '';
+// for(var j = 0; j < 4; j++){
+//     if(bakry_item[j] == user){
+//         flag = true;
+//     }
+// }
+// if(flag){
+//     alert(user + ' is availible at index '+' in our bakery ');
+// }
+// else{
+//     alert('We are sorry. ' +user+ 'is not available in our bakery ')
+// }
+
+                                                                    // QUESTION NO 8
+// var A = [24, 53, 78, 91, 12];
+// document.write('The largest number is '+ A [3] );
+
+                                                                // QUESTION NO 9
+// var A = [24, 53, 78, 91, 12];
+// document.write('The smallest number is ' + A [4]);
+
+                                                                // QUESTION NO 10
+// for(var i =1; i <= 20; i++){
+//     document.write('<b>'+ 5 * i +'<b>'+',')
+// }
